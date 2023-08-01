9b25d33e559fba13276814174504fe60ce491a7b
Eunchi <80859943+Eunchi@users.noreply.github.com>
2023-07-31 17:09:51 -0700

    Create Livestock Lineup.java

---
+++ 2019 Bronze - December/Livestock Lineup.java
@@ -0,0 +1,70 @@
+package pack;
+
+import java.util.*;
+import java.io.*;
+
+public class lineup {
+
+	final public static String[] cows = {"Beatrice", "Belinda", "Bella", "Bessie", "Betsy", "Blue", "Buttercup", "Sue"};
+
+	public static int n;
+	public static int[][] list;
+	
+	public static void main(String[] args) throws Exception {
+		
+		HashMap<String,Integer> m = new HashMap<String,Integer>();
+		for (int i = 0; i < cows.length; i++) m.put(cows[i], i);
+		
+		Scanner stdin = new Scanner(new File("lineup.in"));
+		n = stdin.nextInt();
+		
+		list = new int[n][2];
+		for (int i = 0; i < n; i++) {
+			list[i][0] = m.get(stdin.next());
+			for (int j = 0; j < 4; j++) stdin.next();
+			list[i][1] = m.get(stdin.next());
+		}
+		
+		int[] perm = new int[cows.length];
+		boolean[] used = new boolean[cows.length];
+		go(perm, used, 0);
+		
+		PrintWriter out = new PrintWriter(new FileWriter("lineup.out"));
+		for (int i = 0; i < cows.length; i++)
+			out.println(cows[perm[i]]);
+		out.close();		
+		stdin.close();
+	}
+	
+	public static boolean go(int[] perm, boolean[] used, int k) {
+		
+		if (k == perm.length) return valid(perm);
+			
+		for (int i = 0; i < perm.length; i++) {
+			if (!used[i]) {
+				perm[k] = i;
+				used[i] = true;
+				boolean tmp = go(perm, used, k + 1);
+				if (tmp) return true;
+				used[i] = false;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static boolean valid(int[] perm) {
+		
+		int[] indexOf = new int[perm.length];
+		for (int i = 0; i < perm.length; i++) {
+			indexOf[perm[i]] = i; 
+		}
+		
+		for (int i = 0; i < n; i++) 
+			if (Math.abs(indexOf[list[i][0]]-indexOf[list[i][1]]) != 1) {
+				return false;
+			}
+
+		return true;
+	}
+}
